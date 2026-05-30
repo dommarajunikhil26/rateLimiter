9740282b4ba2084ef29c73a02873c5123e3313ea
dommarajunikhil <nikhild2611@gmail.com>
2026-05-30 12:01:33 -0700

    added SlidingWindowRateLimiter

--- src/main/java/com/nikhil/rateLimiter/aspect/RateLimiterAspect.java
+++ src/main/java/com/nikhil/rateLimiter/aspect/RateLimiterAspect.java
@@ -1,16 +1,22 @@
 package com.nikhil.rateLimiter.aspect;
 
 import com.nikhil.rateLimiter.annotation.RateLimit;
+import com.nikhil.rateLimiter.exception.RateLimitExceededException;
 import com.nikhil.rateLimiter.service.SlidingWindowRateLimiter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.lang.invoke.MethodHandles;
 
 @Aspect
-@Component
 public class RateLimiterAspect {
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final SlidingWindowRateLimiter slidingWindowRateLimiter;
 
     public RateLimiterAspect(SlidingWindowRateLimiter slidingWindowRateLimiter) {
@@ -22,9 +28,21 @@ public class RateLimiterAspect {
         int limit = rateLimit.limit();
         int windowSeconds = rateLimit.windowSeconds();
 
-        HttpServletRequest request = (HttpServletRequest) joinPoint.getThis();
-        String ipAddress = request.getRemoteAddr();
+        ServletRequestAttributes attributes = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes());
+        if (attributes == null) {
+            return;
+        }
+        HttpServletRequest request = attributes.getRequest();
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if(ipAddress == null || ipAddress.isEmpty()){
+            ipAddress = request.getRemoteAddr();
+        }
+        String methodName = joinPoint.getSignature().toShortString();
+        String key = "rate_limit:" + ipAddress + ":" + methodName;
 
-        slidingWindowRateLimiter.checkRateLimiting(ipAddress, limit, windowSeconds);
+        boolean allowed = slidingWindowRateLimiter.checkRateLimiting(key, limit, windowSeconds);
+        if(!allowed) {
+            throw new RateLimitExceededException("Too many requests");
+        }
     }
 }
--- src/main/java/com/nikhil/rateLimiter/service/SlidingWindowRateLimiter.java
+++ src/main/java/com/nikhil/rateLimiter/service/SlidingWindowRateLimiter.java
@@ -1,9 +1,10 @@
 package com.nikhil.rateLimiter.service;
 
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
 
-@Service
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 public class SlidingWindowRateLimiter {
 
     private final RedisTemplate<String, String> redisTemplate;
@@ -12,7 +13,16 @@ public class SlidingWindowRateLimiter {
         this.redisTemplate = redisTemplate;
     }
 
-    public void checkRateLimiting(String ipAddress, int limit, int windowSeconds) {
-        //
+    public boolean checkRateLimiting(String key, int limit, int windowSeconds) {
+        long now = System.currentTimeMillis();
+        long range = now - (windowSeconds * 1000L);
+        redisTemplate.opsForZSet().removeRangeByScore(key, 0, range);
+        Long requestsRemaining = redisTemplate.opsForZSet().zCard(key);
+        if(requestsRemaining != null && requestsRemaining >= limit){
+            return false;
+        }
+        redisTemplate.opsForZSet().add(key, String.valueOf(now) + ":" + UUID.randomUUID(), now);
+        redisTemplate.expire(key, windowSeconds, TimeUnit.SECONDS);
+        return true;
     }
 }
